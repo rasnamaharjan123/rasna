7692063e16569075d9deb869c8debf65dc47584a
rasnamaharjan123 <csit80117733_rasna@achsnepal.edu.np>
2024-06-13 21:25:13 +0545

    Add files via upload

---
+++ num 4.cpp
@@ -0,0 +1,28 @@
+int modInverse(int a, int m) {
+}
+
+// Function to solve the linear congruence equation ax ≡ b (mod m)
+void solveLinearCongruence(int a, int b, int m) {
+    int inverse = modInverse(a, m);
+    
+    if (inverse == -1) {
+        printf("Linear congruence has no solution.\n");
+    } else {
+        // The solution is given by the formula x ≡ b * a^(-1) (mod m)
+        int x = (inverse * b) % m;
+        printf("The solution to the linear congruence is x ≡ %d (mod %d)\n", x, m);
+    }
+}
+
+int main() {
+    int a, b, m;
+    
+    
+    printf("Enter values for a, b, and m(linear congruence equation ax ≡ b (mod m) : ");
+    scanf("%d %d %d", &a, &b, &m);
+    
+    
+    solveLinearCongruence(a, b, m);
+    
+    return 0;
+}
